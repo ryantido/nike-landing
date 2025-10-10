0f0d063fda2b488a7340f999a2a657d6badd997c
ryantido34 <ryantido34@gmail.com>
2025-10-10 01:48:54 +0100

    chore:add popular shoes constants

---
+++ constants/popular-shoes.ts
@@ -0,0 +1,26 @@
+export const PopularShoes = [
+  {
+    name: "Nike Air Max 270",
+    image: "/jeff-tumale-SD9Jyl1xNQ4-unsplash.jpg",
+    price: 199,
+    rating: 4.5,
+  },
+  {
+    name: "Nike Air Max 700",
+    image: "/george-catalina-1vHAP0oeUQE-unsplash.jpg",
+    price: 599,
+    rating: 4.9,
+  },
+  {
+    name: "Nike Air Max 900",
+    image: "/usama-akram-kP6knT7tjn4-unsplash.jpg",
+    price: 299,
+    rating: 4.2,
+  },
+  {
+    name: "Nike Air Vapormax",
+    image: "/bhautik-patel-ui8yd5Qxv-Y-unsplash.jpg",
+    price: 699,
+    rating: 4.8,
+  },
+] as const;
