1decf7472276a2a769fd1f678af030991fcf3257
ryantido34 <ryantido34@gmail.com>
2025-10-10 01:51:09 +0100

    chore:add popular section in the root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import HeroSection from "@/components/HeroSection";
+import HeroSection from "@/components/sections/HeroSection";
+import PopularSection from "@/components/sections/PopularSection";
 
 export default function MainPage() {
   return (
     <div className="min-h-screen">
       <HeroSection />
+      <PopularSection />
     </div>
   );
 }
